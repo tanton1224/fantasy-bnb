5c4973d939542ddd2d1b18d2c37e962584e78a1a
Thomas Anton <thomasanton1224@gmail.com>
2022-07-03 18:14:47 -0400

    added route to get all spots /spots

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,12 +12,15 @@ router.get("/api/csrf/restore", (req, res) => {
 const apiRouter = require('./api');
 const loginRouter = require('./login')
 const signupRouter = require('./signup')
+const spotsRouter = require('./spots')
 
 router.use('/api', apiRouter);
 
 router.use('/login', loginRouter);
 
 router.use('/signup', signupRouter);
+
+router.use('/spots', spotsRouter);
 
 
 module.exports = router;
---
+++ backend/routes/spots.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const { setTokenCookie, restoreUser } = require('../utils/auth');
+const { User, Spot } = require('../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../utils/validation');
+
+const router = express.Router();
+
+
+router.get(
+  '/',
+  async (req, res, next) => {
+    const allSpots = await Spot.findAll()
+
+    res.json(allSpots);
+  }
+  )
+
+
+module.exports = router;
