077f74f77f3161d8fd4776078853e16f0720d22b
Thomas Anton <thomasanton1224@gmail.com>
2022-07-03 14:12:44 -0400

    moved sign up functionality to /signup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,9 +11,13 @@ router.get("/api/csrf/restore", (req, res) => {
 
 const apiRouter = require('./api');
 const loginRouter = require('./login')
+const signupRouter = require('./signup')
 
 router.use('/api', apiRouter);
 
 router.use('/login', loginRouter);
+
+router.use('/signup', signupRouter);
+
 
 module.exports = router;
