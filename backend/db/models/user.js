77c81237a64e7cfcebb9440b951dcf57f1d5bfb2
Thomas Anton <thomasanton1224@gmail.com>
2022-07-03 18:07:17 -0400

    added spots table and initial validations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -44,6 +44,10 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
+      User.hasMany(
+        models.Spot,
+        { foreignKey: 'ownerId', onDelete: 'CASCADE',  hooks: true }
+      )
     }
   };
 
