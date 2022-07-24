501b8b4509ac3eb8949628e9debc9b69b941b3da
Thomas Anton <thomasanton1224@gmail.com>
2022-07-24 18:05:51 -0400

    modal for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-// frontend/src/App.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage/index";
-import SignupFormPage from "./components/SignUpFormPage";
-import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import SignupFormPage from "./components/SignUpFormPage"
+import * as sessionActions from "./store/session";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,14 +12,11 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
+  return (
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
