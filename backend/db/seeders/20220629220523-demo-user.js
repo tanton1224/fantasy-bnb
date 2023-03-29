42fde03bda59ad8280a944af57854c0f61877d55
Thomas Anton <thomasanton1224@gmail.com>
2023-03-29 09:03:06 -0400

    render deployment changes

--- backend/db/seeders/20220629220523-demo-user.js
+++ backend/db/seeders/20220629220523-demo-user.js
@@ -1,8 +1,15 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define schema in options object
+}
+
 const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users'
     return queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
